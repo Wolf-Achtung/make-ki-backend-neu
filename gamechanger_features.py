dc98a9c4fbf844dd20ad4d5dd9db9f0adb64651c
Wolf-Achtung <wolf.hohl@web.de>
2025-09-15 12:05:52 +0200

    Add files via upload

---
+++ gamechanger_features.py
@@ -0,0 +1,15 @@
+# gamechanger_features.py
+
+GAMECHANGER_FEATURES = {
+    "moonshot": True,
+    "maturity_benchmark": True,
+    "foerder_forecast": True,
+    "next_steps": True,
+    "realtime_check": True,
+    "best_practice": True,
+}
+
+# You can disable individual Gamechanger blocks by setting the corresponding
+# flag above to False.  All blocks are enabled by default in the Gold
+# Standard.  These flags control which sections appear in the
+# "Innovation & Gamechanger" chapter of the PDF report.
